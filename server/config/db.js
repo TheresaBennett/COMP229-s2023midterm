2e089cf926519537c5818c381161964eb83f8f5e
Theresa Bennett <tbenne21@my.centennialcollege.ca>
2023-06-21 15:48:59 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,10 @@
+/* FILE: db.js
+   NAME: Theresa Bennett
+   STUDENT ID: 300909345
+   WEB APP NAME: COMP229-S2023-MidTerm-300909345
+   */
+   module.exports = {
+    //local MongoDB deployment ->
+    //"URI": "mongodb://localhost/books229"
+    "URI": "mongodb+srv://TBENNETT:AIcjVs62IPrwy76M@mongodbserver.fnerpqo.mongodb.net/books229?retryWrites=true&w=majority"
+  };
